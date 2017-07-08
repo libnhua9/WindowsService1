5f23f80e5a22121350c73d64bded31ded4e77d41
lbh <lbh@DESKTOP-GF9037E>
2017-07-08 12:27:25 +0800

    添加项目文件。

---
+++ WindowsService1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace WindowsService1
+{
+    static class Program
+    {
+        /// <summary>
+        /// 应用程序的主入口点。
+        /// </summary>
+        static void Main()
+        {
+
+            Service1 s = new Service1();
+            s.onLoad();
+            System.Threading.Thread.Sleep(System.Threading.Timeout.Infinite);
+
+        }
+      
+    }
+}
---
+++ WindowsService1/Service1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.ServiceProcess;
+using System.Timers;
+namespace WindowsService1
+{
+    public partial class Service1 : ServiceBase
+    {
+         Timer timer;
+        public Service1()
+        {
+            InitializeComponent();
+        }
+
+        public void onLoad()
+        {
+            OnStart(null);
+        }
+
+        protected override void OnStart(string[] args)
+        {
+
+            timer = new Timer( );
+            timer.Interval = 1000 * 6;
+            timer.Elapsed += new ElapsedEventHandler(timer_Elapsed);
+            timer.AutoReset = true;
+            try
+            {
+                timer.Start();
+            }
+            catch (ArgumentOutOfRangeException Ex)
+            {
+                throw Ex;
+            }
+        }
+        private void timer_Elapsed(object obj, EventArgs args)
+        {
+            //using (FileStream fs = new FileStream(@"E:\test.txt", FileMode.Append, FileAccess.Write))
+            //{
+                //using (StreamWriter sw = new StreamWriter(fs))
+                //{
+                    var resultContent = "";
+                    //https://way.jd.com/jisuapi/weather?city=深圳&cityid=111&citycode=101260301&appkey=101260301&appkey=ccba4c1751078e7be9caa133a3c2439c
+                    using (DB_TestEntities db = new DB_TestEntities())
+                    {
+                        var data = db.Tb_WeatherForecast.ToList();
+                        foreach (var item in data)
+                        {
+                            using (HttpClient client = new HttpClient())
+                            {
+                                var url = "https://way.jd.com/jisuapi/weather?";
+                                var postData = $"city={item.City}&cityid=111&citycode=101260301&appkey=ccba4c1751078e7be9caa133a3c2439c";
+                                var result = client.PostAsync(url + postData, null).Result;
+                                resultContent = result.Content.ReadAsStringAsync().Result;
+                                //Console.WriteLine(resultContent);
+                            }
+                            if (item != null)
+                            {
+                                item.Config = resultContent;
+                                item.Verion += 1;
+                                item.UpdateDate = DateTime.Now;
+                                db.Entry(item).State = System.Data.Entity.EntityState.Modified;
+                            }
+                        }
+                        db.SaveChanges();
+                    }
+                //    sw.WriteLine(DateTime.Now.ToString());
+                //}
+            //}
+
+        }
+        protected override void OnStop()
+        {
+        }
+
+    }
+}
